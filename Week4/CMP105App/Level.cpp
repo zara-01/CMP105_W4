03635760962cfbd5c3b8911a1f7751f383ba5968
Paul Robertson <p.robertson@abertay.ac.uk>
2020-02-04 12:00:34 +0000

    Initial Commit
    Added example project

---
+++ Week4/CMP105App/Level.cpp
@@ -0,0 +1,59 @@
+#include "Level.h"
+
+Level::Level(sf::RenderWindow* hwnd, Input* in)
+{
+	window = hwnd;
+	input = in;
+
+	// initialise game objects
+	texture.loadFromFile("gfx/Mushroom.png");
+
+	testSprite.setTexture(&texture);
+	testSprite.setSize(sf::Vector2f(100, 100));
+	testSprite.setPosition(100, 100);
+
+}
+
+Level::~Level()
+{
+
+}
+
+// handle user input
+void Level::handleInput(float dt)
+{
+	// Close window on Escape pressed.
+	if (input->isKeyDown(sf::Keyboard::Escape))
+	{
+		window->close();
+	}
+
+}
+
+// Update game objects
+void Level::update(float dt)
+{
+	
+}
+
+// Render level
+void Level::render()
+{
+	beginDraw();
+
+	window->draw(testSprite);
+
+	endDraw();
+}
+
+// clear back buffer
+void Level::beginDraw()
+{
+	window->clear(sf::Color(100, 149, 237));
+}
+
+// Ends rendering to the back buffer, and swaps buffer to the screen.
+void Level::endDraw()
+{
+	window->display();
+}
